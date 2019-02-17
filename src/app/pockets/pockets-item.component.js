ea493ce9203dd0aa7ea8175baf753013dd357fc6
Piotr <fert.piotr@gmail.com>
2019-02-17 17:02:29 +0100

    Show top up dialog

--- src/app/pockets/pockets-item.component.js
+++ src/app/pockets/pockets-item.component.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Card, CardActions, CardContent, Typography, withStyles } from '@material-ui/core';
 
 import { TransactionsContainer } from '../transactions/transactions.container';
+import { TopUpComponent } from '../top-up/top-up.component';
 
 const styles = () => ({
     card: {
@@ -15,19 +16,57 @@ const styles = () => ({
     }
 });
 
-const PocketItemComponent = ({ pocket, classes }) => (
-    <Card className={classes.card}>
-        <div>
-            <Typography variant="h3" className={classes.header}>{pocket.symbol} {pocket.balance}</Typography>
-        </div>
-        <CardActions>
-            <Button color="inherit">Top Up</Button>
-            <Button color="inherit">Exchange</Button>
-        </CardActions>
-        <CardContent>
-            <TransactionsContainer pocketId={pocket.id}/>
-        </CardContent>
-    </Card>
-);
+class PocketItemComponent extends Component {
+    
+    state = {
+        isDialogOpened: false,
+    };
+    
+    openDialog = () => {
+        this.setState({ isDialogOpened: true });
+    };
+    
+    closeDialog = () => {
+        this.setState({ isDialogOpened: false });
+    };
+    
+    onBackdropClick = () => {
+        this.closeDialog();
+    };
+    
+    onConfirm = (topUpAmount) => {
+        this.closeDialog();
+    };
+    
+    onCancel = () => {
+        this.closeDialog();
+    };
+    
+    render() {
+        const { pocket, classes } = this.props;
+        
+        return (
+            <Card className={classes.card}>
+                <div>
+                    <Typography variant="h3" className={classes.header}>{pocket.symbol} {pocket.balance}</Typography>
+                </div>
+                <CardActions>
+                    <Button color="inherit" onClick={this.openDialog}>Top Up</Button>
+                    <Button color="inherit">Exchange</Button>
+                </CardActions>
+                <CardContent>
+                    <TransactionsContainer pocketId={pocket.id}/>
+                </CardContent>
+                
+                <TopUpComponent
+                    isDialogOpened={this.state.isDialogOpened}
+                    onBackdropClick={this.onBackdropClick}
+                    onConfirm={this.onConfirm}
+                    onCancel={this.onCancel}
+                />
+            </Card>
+        );
+    }
+}
 
 export default withStyles(styles)(PocketItemComponent);
--- src/app/top-up/top-up.component.js
+++ src/app/top-up/top-up.component.js
@@ -1,8 +1,47 @@
-import React from 'react';
-import { Dialog, DialogTitle } from '@material-ui/core';
+import React, { Component } from 'react';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@material-ui/core';
 
-export const TopUpComponent = () => (
-    <Dialog open={true}>
-        <DialogTitle>Top up your pocket</DialogTitle>
-    </Dialog>
-);
+export class TopUpComponent extends Component {
+    
+    state = {
+        topUpAmount: 0,
+        fullWidth: true,
+        maxWidth: 'sm',
+    };
+    
+    onChangeHandler = topUpAmount => event => {
+        console.log(topUpAmount, event);
+        this.setState({ [topUpAmount]: event.target.value });
+    };
+    
+    render() {
+        const { isDialogOpened, onBackdropClick, onConfirm, onCancel } = this.props;
+        
+        return (
+            <Dialog
+                open={isDialogOpened}
+                onBackdropClick={onBackdropClick}
+                fullWidth={this.state.fullWidth}
+                maxWidth={this.state.maxWidth}
+            >
+                <DialogTitle>Top up your pocket</DialogTitle>
+                <DialogContent>
+                    <form noValidate autoComplete="off">
+                        <TextField
+                            id="top-up-amount"
+                            label="Top Up amount"
+                            value={this.state.topUpAmount}
+                            onChange={this.onChangeHandler('topUpAmount')}
+                            type="number"
+                            fullWidth
+                        />
+                    </form>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={onCancel}>Close</Button>
+                    <Button color="secondary" onClick={() => onConfirm(this.state.topUpAmount)}>Top Up</Button>
+                </DialogActions>
+            </Dialog>
+        );
+    }
+}
